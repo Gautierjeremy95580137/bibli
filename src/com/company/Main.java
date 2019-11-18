ad505a99b65c919e1bdef2de513accd13e96fe7d
jeremy <je.gautier137@gmail.com>
2019-11-18 14:47:27 +0100

    fenetre initialisé

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
  JFrame fenetre= new JFrame();
- fenetre.setVisible(true);
+        // definition du titre
+        fenetre.setTitle("Ma bibliothèque");
+        // définit sa taille
+        fenetre.setSize(400, 100);
+        // nous demandons a notre fenetre de se positionner au centre
+        fenetre.setLocationRelativeTo(null);
+        //termine le processus avec la croix rouge
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //fenetre visible
+        fenetre.setVisible(true);
     }
 
 }
