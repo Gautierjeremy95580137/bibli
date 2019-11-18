aebd71e818d1a8167b1fbab9dd6618455fc8468a
jeremy <je.gautier137@gmail.com>
2019-11-18 14:07:53 +0100

    commit pour laurent

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// laurent t'es nul
+        System.out.println("laurent t'es trop beau");
     }
 }
