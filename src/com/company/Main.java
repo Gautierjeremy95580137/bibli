eef4bbda835517eabbb24bdf4362343353ed1006
laurent <laurent.bodin58@gmail.com>
2019-11-19 15:18:57 +0100

    user story #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,19 @@ public class Main extends JFrame{
 
     public Main(){
         super("Bibliothèque");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        JPanel myPanel = new JPanel();
+        this.setContentPane(myPanel);
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        JMenu file = new JMenu("Fichier");
+        menuBar.add(file);
+        JMenuItem open = new JMenuItem("Ouvrir");
+        file.add(open);
+
+
+        GridBagLayout myLayout = new GridBagLayout();
+        myPanel.setLayout(myLayout);
 
     }
 
@@ -17,6 +30,7 @@ public class Main extends JFrame{
     Main myWindow = new Main();
     myWindow.pack();
     // rend la fenêtre visible
+    myWindow.setSize(500,400);
     myWindow.setVisible(true);
     }
 
